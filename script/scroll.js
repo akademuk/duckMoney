b6a34254c89a9849048d45da40b417d18b3b5e34
akademuk <akademuk24@gmail.com>
2023-04-18 10:23:39 +0300

    sss

--- script/scroll.js
+++ script/scroll.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {
-    $(".informationScroll").mCustomScrollbar({
-      theme: "dark",
-      scrollInertia: 200,
-      axis: "y",
-      scrollbarPosition: "inside",
-      mouseWheel: {
-        enable: true,
-        scrollAmount: 200
-      }
-    });
-  });
+  var $scrollbar = $(".informationScroll");
+  var $bar = $scrollbar.find(".mCSB_dragger_bar"); // находим полосу скроллбара
   
+  $scrollbar.mCustomScrollbar({
+    theme: "dark",
+    scrollInertia: 200,
+    axis: "y",
+    scrollbarPosition: "inside",
+    mouseWheel: {
+      enable: true,
+      scrollAmount: 200
+    },
+    callbacks: {
+      whileScrolling: function() {
+        var top = Math.abs(parseInt($bar.css("top"))); // находим расстояние от начала полосы до верхней границы родительского блока в пикселях
+        var height = $scrollbar.height(); // находим высоту родительского блока в пикселях
+        var progress = top / height; // вычисляем прогресс в долях
+        var yellow = Math.round(255 * progress); // находим значение желтого цвета в диапазоне от 0 до 255
+        
+        $bar.css("background-color", "rgb(255, " + yellow + ", 0)"); // устанавливаем цвет полосы в зависимости от прогресса
+      }
+    }
+  });
+});
+
